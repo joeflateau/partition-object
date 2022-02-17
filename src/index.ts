62bfd61d4047efd5f7d73e5c44c49ae1a4f09314
Joe Flateau <joe@joeflateau.net>
2022-02-17 12:22:10 -0500

    looser obj requirement

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { omit, pick } from 'lodash';
 
-export function partitionObject<
-  T extends Record<string, unknown>,
-  TKey extends keyof T
->(obj: T, keys: TKey[]): [Pick<T, TKey>, Omit<T, TKey>] {
+export function partitionObject<T extends object, TKey extends keyof T>(
+  obj: T,
+  keys: TKey[]
+): [Pick<T, TKey>, Omit<T, TKey>] {
   const objWith = pick(obj, keys);
   const objWithout = omit(obj, keys);
   return [objWith, objWithout];
